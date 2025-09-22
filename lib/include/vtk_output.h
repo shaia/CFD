1929a19638d6866d761f83e99ebf5fcec1a7a6d9
shaia <shaia@outlook.com>
2025-09-22 17:30:12 +0300

    fixing error in missing declaration in vtk

--- lib/include/vtk_output.h
+++ lib/include/vtk_output.h
@@ -2,9 +2,20 @@
 #define VTK_OUTPUT_H
 
 #include <stddef.h>
+#include "solver.h"
 
-void write_vtk_output(const char* filename, const char* field_name, 
+void write_vtk_output(const char* filename, const char* field_name,
                      const double* data, size_t nx, size_t ny,
                      double xmin, double xmax, double ymin, double ymax);
+
+void write_vtk_vector_output(const char* filename, const char* field_name,
+                           const double* u_data, const double* v_data,
+                           size_t nx, size_t ny,
+                           double xmin, double xmax, double ymin, double ymax);
+
+void write_vtk_flow_field(const char* filename,
+                         const FlowField* field,
+                         size_t nx, size_t ny,
+                         double xmin, double xmax, double ymin, double ymax);
 
 #endif // VTK_OUTPUT_H
