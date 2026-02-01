9827b7b3de8b9bb56abf2a363169d580ea3db3f4
shaia <shaia@outlook.com>
2026-02-01 15:59:38 +0200

    refactor: Extract common outlet SIMD template for AVX2/NEON
    Create boundary_conditions_outlet_simd.h with parameterized SIMD
    implementation. AVX2 and NEON outlet files now define macros for
    intrinsics/width and include the shared template.

--- lib/src/boundary/avx2/boundary_conditions_outlet_avx2.c
+++ lib/src/boundary/avx2/boundary_conditions_outlet_avx2.c
@@ -1,130 +1,26 @@
 /**
  * Outlet Boundary Conditions - AVX2 + OpenMP Implementation
  *
- * Combined AVX2 SIMD and OpenMP parallelized outlet boundary condition implementation.
- * Uses OpenMP for thread-level parallelism across rows.
- * Uses AVX2 SIMD for instruction-level parallelism on contiguous memory.
- *
- * Supports:
- * - Zero-gradient (Neumann) outlet
- * - Convective outlet (advection-based)
+ * Delegates to parameterized SIMD template with AVX2 intrinsics.
  */
-
-#include "../boundary_conditions_outlet_common.h"
 
 #if defined(CFD_HAS_AVX2) && defined(CFD_ENABLE_OPENMP)
 #define BC_HAS_AVX2 1
 #include <immintrin.h>
 #include <omp.h>
-#include <limits.h>
-#endif
 
-#if defined(BC_HAS_AVX2)
+#define BC_SIMD_STORE(dst, val) _mm256_storeu_pd((dst), (val))
+#define BC_SIMD_LOAD(src)       _mm256_loadu_pd((src))
+#define BC_SIMD_WIDTH           4
+#define BC_SIMD_MASK            3
+#define BC_SIMD_THRESHOLD       256
+#define BC_OUTLET_FUNC_NAME     bc_apply_outlet_avx2_impl
 
-#define BC_AVX2_THRESHOLD 256
-
-static inline int size_to_int(size_t sz) {
-    return (sz > (size_t)INT_MAX) ? INT_MAX : (int)sz;
-}
-
-cfd_status_t bc_apply_outlet_avx2_impl(double* field, size_t nx, size_t ny,
-                                            const bc_outlet_config_t* config) {
-    if (!field || !config || nx < 3 || ny < 3) {
-        return CFD_ERROR_INVALID;
-    }
-
-    if (!bc_outlet_is_valid_edge(config->edge)) {
-        return CFD_ERROR_INVALID;
-    }
-
-    if (!bc_outlet_is_valid_type(config->type)) {
-        return CFD_ERROR_INVALID;
-    }
-
-    /* For left/right edges, use OMP over rows.
-     * For top/bottom edges, use AVX2 on contiguous memory. */
-    int j, i;
-
-    switch (config->type) {
-        case BC_OUTLET_ZERO_GRADIENT:
-        case BC_OUTLET_CONVECTIVE:
-            /* Both types use zero-gradient for now. */
-            switch (config->edge) {
-                case BC_EDGE_LEFT:
-                    #pragma omp parallel for schedule(static)
-                    for (j = 0; j < size_to_int(ny); j++) {
-                        size_t row = (size_t)j * nx;
-                        field[row] = field[row + 1];
-                    }
-                    break;
-
-                case BC_EDGE_RIGHT:
-                    #pragma omp parallel for schedule(static)
-                    for (j = 0; j < size_to_int(ny); j++) {
-                        size_t row = (size_t)j * nx;
-                        field[row + nx - 1] = field[row + nx - 2];
-                    }
-                    break;
-
-                case BC_EDGE_BOTTOM: {
-                    /* Bottom: row 0 = row 1 */
-                    double* dst = field;
-                    double* src = field + nx;
-                    size_t simd_end = nx & ~(size_t)3;
-
-                    if (nx >= BC_AVX2_THRESHOLD) {
-                        #pragma omp parallel for schedule(static)
-                        for (i = 0; i < size_to_int(simd_end); i += 4) {
-                            _mm256_storeu_pd(dst + i, _mm256_loadu_pd(src + i));
-                        }
-                    } else {
-                        for (size_t ii = 0; ii < simd_end; ii += 4) {
-                            _mm256_storeu_pd(dst + ii, _mm256_loadu_pd(src + ii));
-                        }
-                    }
-                    /* Remainder */
-                    for (size_t ii = simd_end; ii < nx; ii++) {
-                        dst[ii] = src[ii];
-                    }
-                    break;
-                }
-
-                case BC_EDGE_TOP: {
-                    /* Top: row ny-1 = row ny-2 */
-                    double* dst = field + ((ny - 1) * nx);
-                    double* src = field + ((ny - 2) * nx);
-                    size_t simd_end = nx & ~(size_t)3;
-
-                    if (nx >= BC_AVX2_THRESHOLD) {
-                        #pragma omp parallel for schedule(static)
-                        for (i = 0; i < size_to_int(simd_end); i += 4) {
-                            _mm256_storeu_pd(dst + i, _mm256_loadu_pd(src + i));
-                        }
-                    } else {
-                        for (size_t ii = 0; ii < simd_end; ii += 4) {
-                            _mm256_storeu_pd(dst + ii, _mm256_loadu_pd(src + ii));
-                        }
-                    }
-                    /* Remainder */
-                    for (size_t ii = simd_end; ii < nx; ii++) {
-                        dst[ii] = src[ii];
-                    }
-                    break;
-                }
-
-                default:
-                    return CFD_ERROR_INVALID;
-            }
-            break;
-
-        default:
-            return CFD_ERROR_INVALID;
-    }
-
-    return CFD_SUCCESS;
-}
+#include "../boundary_conditions_outlet_simd.h"
 
 #else /* !BC_HAS_AVX2 */
+
+#include "../boundary_conditions_outlet_common.h"
 
 cfd_status_t bc_apply_outlet_avx2_impl(double* field, size_t nx, size_t ny,
                                             const bc_outlet_config_t* config) {
---
+++ lib/src/boundary/boundary_conditions_outlet_simd.h
@@ -0,0 +1,109 @@
+/**
+ * Outlet Boundary Conditions - Parameterized SIMD Implementation
+ *
+ * This header is included by both AVX2 and NEON outlet implementations.
+ * Before including, define:
+ *   BC_SIMD_STORE(dst, val)  - store SIMD vector to memory
+ *   BC_SIMD_LOAD(src)        - load SIMD vector from memory
+ *   BC_SIMD_WIDTH            - number of doubles per SIMD vector (4 or 2)
+ *   BC_SIMD_MASK             - alignment mask (~3 for AVX2, ~1 for NEON)
+ *   BC_SIMD_THRESHOLD        - min width for OMP on SIMD loops
+ *   BC_OUTLET_FUNC_NAME      - function name to define
+ */
+
+#include "boundary_conditions_outlet_common.h"
+#include <limits.h>
+
+static inline int bc_outlet_simd_size_to_int(size_t sz) {
+    return (sz > (size_t)INT_MAX) ? INT_MAX : (int)sz;
+}
+
+static cfd_status_t BC_OUTLET_FUNC_NAME(double* field, size_t nx, size_t ny,
+                                         const bc_outlet_config_t* config) {
+    if (!field || !config || nx < 3 || ny < 3) {
+        return CFD_ERROR_INVALID;
+    }
+
+    if (!bc_outlet_is_valid_edge(config->edge)) {
+        return CFD_ERROR_INVALID;
+    }
+
+    if (!bc_outlet_is_valid_type(config->type)) {
+        return CFD_ERROR_INVALID;
+    }
+
+    int j, i;
+
+    switch (config->type) {
+        case BC_OUTLET_ZERO_GRADIENT:
+        case BC_OUTLET_CONVECTIVE:
+            switch (config->edge) {
+                case BC_EDGE_LEFT:
+                    #pragma omp parallel for schedule(static)
+                    for (j = 0; j < bc_outlet_simd_size_to_int(ny); j++) {
+                        size_t row = (size_t)j * nx;
+                        field[row] = field[row + 1];
+                    }
+                    break;
+
+                case BC_EDGE_RIGHT:
+                    #pragma omp parallel for schedule(static)
+                    for (j = 0; j < bc_outlet_simd_size_to_int(ny); j++) {
+                        size_t row = (size_t)j * nx;
+                        field[row + nx - 1] = field[row + nx - 2];
+                    }
+                    break;
+
+                case BC_EDGE_BOTTOM: {
+                    double* dst = field;
+                    double* src = field + nx;
+                    size_t simd_end = nx & ~(size_t)BC_SIMD_MASK;
+
+                    if (nx >= BC_SIMD_THRESHOLD) {
+                        #pragma omp parallel for schedule(static)
+                        for (i = 0; i < bc_outlet_simd_size_to_int(simd_end); i += BC_SIMD_WIDTH) {
+                            BC_SIMD_STORE(dst + i, BC_SIMD_LOAD(src + i));
+                        }
+                    } else {
+                        for (size_t ii = 0; ii < simd_end; ii += BC_SIMD_WIDTH) {
+                            BC_SIMD_STORE(dst + ii, BC_SIMD_LOAD(src + ii));
+                        }
+                    }
+                    for (size_t ii = simd_end; ii < nx; ii++) {
+                        dst[ii] = src[ii];
+                    }
+                    break;
+                }
+
+                case BC_EDGE_TOP: {
+                    double* dst = field + ((ny - 1) * nx);
+                    double* src = field + ((ny - 2) * nx);
+                    size_t simd_end = nx & ~(size_t)BC_SIMD_MASK;
+
+                    if (nx >= BC_SIMD_THRESHOLD) {
+                        #pragma omp parallel for schedule(static)
+                        for (i = 0; i < bc_outlet_simd_size_to_int(simd_end); i += BC_SIMD_WIDTH) {
+                            BC_SIMD_STORE(dst + i, BC_SIMD_LOAD(src + i));
+                        }
+                    } else {
+                        for (size_t ii = 0; ii < simd_end; ii += BC_SIMD_WIDTH) {
+                            BC_SIMD_STORE(dst + ii, BC_SIMD_LOAD(src + ii));
+                        }
+                    }
+                    for (size_t ii = simd_end; ii < nx; ii++) {
+                        dst[ii] = src[ii];
+                    }
+                    break;
+                }
+
+                default:
+                    return CFD_ERROR_INVALID;
+            }
+            break;
+
+        default:
+            return CFD_ERROR_INVALID;
+    }
+
+    return CFD_SUCCESS;
+}
